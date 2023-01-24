d8ee7a33447162739859b70d6347192ccce54614
Kasper Munch <kaspermunchj@gmail.com>
2023-01-24 15:14:20 +0100

    Add Swift package

---
+++ README.md
@@ -0,0 +1,8 @@
+# NavigationManager
+
+### Why
+In `iOS 16` the `NavigationStack` was introduced. This had made navigation in SwiftUI much more pleasant to use. However, there is still room for improvement. This is where the `NavigationManager` comes in handy.
+
+### Supports
+##### iOS
+`iOS 16` is required and `iOS 16.2` is recommended.
---
+++ Sources/SwiftUINavigationManager/SwiftUINavigationManager.swift
@@ -0,0 +1,49 @@
+import SwiftUI
+
+public class NavigationManager<Route: Hashable>: ObservableObject {
+    @Published var path = NavigationPath()
+    
+    private var stack = [0]
+    
+    public func present(_ view: Route) {
+        path.append(view)
+    }
+    
+    public func presentAndMakeKey(_ view: Route) {
+        stack.append(path.count)
+        path.append(view)
+    }
+    
+    public func popToLastKey() {
+        guard stack.count > 1 else {
+            stack = [0]
+            popToRoot()
+            return
+        }
+        
+        if let last = stack.last {
+            stack.removeLast()
+            path.removeLast(path.count - last)
+        }
+    }
+    
+    public func dismiss() {
+        path.removeLast()
+        if path.count == stack.last {
+            stack.removeLast()
+        }
+    }
+    
+    public func popToRoot() {
+        stack = [0]
+        path.removeLast(path.count)
+    }
+    
+    public func pop(amount: Int) {
+        guard amount <= path.count else {
+            popToRoot()
+            return
+        }
+        path.removeLast(amount)
+    }
+}
